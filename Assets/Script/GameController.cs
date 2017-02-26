96beb1a8332ca18ec256a39f38d2ef18dca96763
yuga <yuga.ichinohe@gmail.com>
2017-02-26 14:45:07 +0800

    initial commit

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public  UnityEngine.UI.Text scoreLabel;
+	public GameObject winnerLabelObject;
+
+	public void Update ()
+	{
+		int count = GameObject.FindGameObjectsWithTag ("Item").Length;
+		scoreLabel.text = count.ToString ();
+
+		if (count == 0) {
+			// オブジェクトをアクティブにする
+			winnerLabelObject.SetActive (true);
+		}
+	}
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	// speedを制御する
+	public float speed = 10;
+
+	void Update ()
+	{
+		float inputX = Input.GetAxis("Horizontal");
+		float inputZ = Input.GetAxis("Vertical");
+
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+		// xとyにspeedを掛ける
+		rigidbody.AddForce(inputX * speed, 0, inputZ * speed);
+	}
+}
